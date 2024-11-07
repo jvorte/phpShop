6db26a5fff7e86419b6313e99d4352e014bcf5cd
Dimitris <jvortelinas@gmail.com>
2024-11-07 13:45:35 +0100

    first

---
+++ includes/nav.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>SuperTshirt</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="style.css" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+      crossorigin="anonymous"
+    />
+  </head>
+  <body>
+    <!-- 1. container -->
+    <div class="container">
+      <!-- navbar section -->
+      <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid ">
+          <h1 class="navbar-brand" href="#">SuperTshirt</h1>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto  mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link " href="store.php">Store</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Products
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><hr class="dropdown-divider" /></li>
+                  <li>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </li>
+         
+                </ul>
+              </li>
+              <li>
+                <i class="bi bi-cart ps-5"  style="font-size: 1.4rem; color: rgb(237, 100, 157);"></i> Basket
+              </li>
+            </ul>
+            <div class="d-flex" role="login">
+       
+              <a class="btn btn-outline-danger" type="submit">
+                Login
+              </a>
+
+            </div>
+          </div>
+        </div>
+      </nav>
+      <!--end  navbar section -->
+            <!-- title section -->
+            <h1 class="text-center mt-5 mainTile">
+        SuperTshirt
+        <span
+          ><img
+            class="imgTitle"
+            src="assets\img\Hawaiian-Flower.png"
+            alt=""
+            srcset=""
+        /></span>
+      </h1>
+      <!--end  title section -->
+
+      <!--paragraph title section -->
+      <h5 class="text-center paragraphTitle">
+        Lorem ipsum dolor sit amet consectetut.
+      </h5>
+      <!--end paragraph  title section -->
+    </div>
+    <!-- end 1. container -->
---
+++ index.php
@@ -0,0 +1,125 @@
+
+<?php include 'includes/nav.php';?>
+
+
+
+    <!-- 2. container -->
+    <div class="container ">
+
+      <nav class="navbar mt-4 bg-light ">
+        <div class="container-fluid ">
+          <h5 class="navbar-text "> Navbar text with an inline element </h5>
+          <h5 class="text-end">WINTER SALES %</h5>
+        </div>
+        
+      </nav>
+
+      <!--  main row -->
+      <div class="row mt-2 justify-content-md-center">
+        <!-- 1. -->
+        <div class="col col-lg-4 ">
+          <div class="card mb-3 mt-5" style="max-width: 540px">
+            <div class="row g-0">
+              <div class="col-md-6">
+                <img
+                  src="assets/img/4.jpg"
+                  class="img-fluid rounded-start"
+                  alt="..."
+                />
+              </div>
+              <div class="col-md-6">
+                <div class="card-body">
+                  <h5 class="card-title mt-4">Men's</h5>
+                  <p class="card-text">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer.
+                  </p>
+                  <p class="card-text">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--end 1. -->
+        <!-- 2. -->
+        <div class="col-xl-4 ">
+
+          <div class="card mb-3 mt-5" style="max-width: 540px">
+            <div class="row g-0">
+              <div class="col-md-6">
+                <img
+                  src="assets/img/5.jpg"
+                  class="img-fluid rounded-start"
+                  alt="..."
+                />
+              </div>
+              <div class="col-md-6">
+                <div class="card-body">
+                  <h5 class="card-title mt-4">Women's</h5>
+                  <p class="card-text">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer.
+                  </p>
+                  <p class="card-text">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <!-- end 2. -->
+        <!-- 3. -->
+        <div class="col-xl-4">
+
+          <div class="card mb-3 mt-5" style="max-width: 540px">
+            <div class="row g-0">
+              <div class="col-md-6">
+                <img
+                  src="assets/img/1.jpg"
+                  class="img-fluid rounded-start"
+                  alt="..."
+                />
+              </div>
+              <div class="col-md-6">
+                <div class="card-body">
+                  <h5 class="card-title mt-4">Accessories</h5>
+                  <p class="card-text">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer.
+                  </p>
+                  <p class="card-text">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <!-- end 3. -->
+      </div>
+      <!-- end main row -->
+
+      
+    </div>
+
+    <!-- end 2. container -->
+
+
+<?php include 'includes/footer.php' ?>
+
+   
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
